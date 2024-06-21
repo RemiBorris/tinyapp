9fe08a435beaebddf84af7975e8ccc6d729d75ea
remiborris <remiborris@gmail.com>
2024-06-21 14:55:45 -0300

    add cookieParser for middleware and use cookie to display username onto the header of our form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser');
 
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
@@ -14,6 +15,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString();
@@ -41,19 +43,28 @@ app.post("/login", (req, res) =>{
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
-
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
